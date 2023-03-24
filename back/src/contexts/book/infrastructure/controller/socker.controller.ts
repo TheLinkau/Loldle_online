0b38aaf54ab3ee9e78932631253dd14696e1b178
leoANDRIAN <leo.andrian@hotmail.com>
2023-03-24 14:53:10 +0100

    [BACK] currentRoomId

--- back/src/contexts/book/infrastructure/controller/socker.controller.ts
+++ back/src/contexts/book/infrastructure/controller/socker.controller.ts
@@ -1,7 +1,11 @@
 import { Server as SocketIOServer } from 'socket.io';
+import shortid from 'shortid';
 
 export class SocketController {
+
     constructor(private readonly socketIOServer: SocketIOServer) { }
+
+    public currentRoomId = "string";
 
     init(): void {
         this.socketIOServer.on('connection', (socket) => {
@@ -15,6 +19,20 @@ export class SocketController {
                 // Send confirmation message to client
                 socket.emit('invitationAccepted', roomId);
                 this.socketIOServer.to(roomId).emit('roomJoined', "a user has joined the room");
+            });
+
+            // Handle creating room
+            socket.on('createRoom', () => {
+                // Generate a random short ID for the room
+                const roomId = shortid.generate();
+                console.log(roomId)
+
+                // Join the game room
+                socket.join(roomId);
+                this.currentRoomId = roomId;
+
+                // Send id for redirection
+                socket.emit('roomCreated', roomId);
             });
 
             // Add more event handlers here as needed
