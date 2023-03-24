33aef797fd68c043382a083b04d6ad93f9fd57e6
nathan <nathan.merigeau@gmail.com>
2023-03-24 10:17:07 +0100

    front svelte

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <style>
+    .bg-image {
+      background-image: url('/src/img/fond.webp');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      background-attachment: fixed;
+      height: 100%;
+      width: 100%;
+    }
+
+    .logo {
+        content: url('/src/img/logo.webp');
+    } 
+    
+    .container {
+      min-height: 100vh;
+    }
+  </style>
+</head>
+
+
+<slot></slot>
---
+++ front/src/routes/game/+page.svelte
@@ -0,0 +1,38 @@
+<div class="bg-image d-flex align-items-center justify-content-center text-white">
+<div class="container">
+
+    <div class="d-flex justify-content-center">
+        <img alt="logo" class="logo">
+    </div>
+    
+    <div class="d-flex justify-content-center">
+        <form id="recherche-form">
+        <div class="mb-3">
+            <input id="champion" placeholder="Type champion name ..." type="text" autocomplete="new-password">
+            <button type="submit" class="btn btn-primary">Jouer</button>
+        </div>
+        </form>
+    </div>
+
+    <table id="resultats-table" class="table mt-3 text-white">
+    <thead>
+        <tr>
+        <th>Champion</th>
+        <th>Gender</th>
+        <th>Positions</th>
+        <th>Species</th>
+        <th>Resource</th>
+        <th>Range type</th>
+        <th>Region(s)</th>
+        <th>Release year</th>
+        <th>À qui de jouer ?</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+    </table>
+
+</div>
+</div>
+
+<script></script>
